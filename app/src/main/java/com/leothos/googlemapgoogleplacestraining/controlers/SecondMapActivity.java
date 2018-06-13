091599bebc2d161f7fef177711dcd413409cd1c6
leothos <soussou.med@live.fr>
2018-06-13 18:06:52 +0200

    Add marker on proximity place from current position

--- app/src/main/java/com/leothos/googlemapgoogleplacestraining/controlers/SecondMapActivity.java
+++ app/src/main/java/com/leothos/googlemapgoogleplacestraining/controlers/SecondMapActivity.java
@@ -13,22 +13,33 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.leothos.googlemapgoogleplacestraining.R;
+import com.leothos.googlemapgoogleplacestraining.models.PlaceInfo;
+import com.leothos.googlemapgoogleplacestraining.models.ProximityPlace;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     //CONSTANT
     public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    public static final float DEFAULT_ZOOM = 18f;
     private static final String TAG = "SecondMapActivity";
-    public static final float DEFAULT_ZOOM = 15f;
+    List<PlaceInfo> mPlaceInfoList = new ArrayList<>();
+    ArrayList<ProximityPlace> mProximityPlaces;
     //VAR
     private GoogleMap mMap;
     private GeoDataClient mGeoDataClient;
@@ -36,8 +47,9 @@ public class SecondMapActivity extends AppCompatActivity implements OnMapReadyCa
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private Location mLastKnownLocation;
     private boolean mLocationPermissionGranted;
-    //Ivry localisation
-    LatLng mDefaultLocation = new LatLng(48.7927684, 2.3591994999999315);
+    private LatLng mDefaultLocation = new LatLng(48.7927684, 2.3591994999999315);
+    private LatLng[] mLikelyPlaceLatlng;
+    private String[] mLikelyPlaceName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +72,7 @@ public class SecondMapActivity extends AppCompatActivity implements OnMapReadyCa
         getLocationPermission();
         getDeviceLocation();
         updateUI();
+        showProximityPlace();
 
 
     }
@@ -115,7 +128,7 @@ public class SecondMapActivity extends AppCompatActivity implements OnMapReadyCa
     }
 
     //---------------------------------------------------------------------------------------------//
-    //                                          UI                                                 //
+    //                                     UI & DEVICE LOCATION                                    //
     //---------------------------------------------------------------------------------------------//
 
     private void updateUI() {
@@ -126,6 +139,7 @@ public class SecondMapActivity extends AppCompatActivity implements OnMapReadyCa
             if (mLocationPermissionGranted) {
                 mMap.setMyLocationEnabled(true);
                 mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                showProximityPlace();
             } else {
                 mMap.setMyLocationEnabled(false);
                 mMap.getUiSettings().setMyLocationButtonEnabled(false);
@@ -154,7 +168,7 @@ public class SecondMapActivity extends AppCompatActivity implements OnMapReadyCa
                                             mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
                         } else {
                             Log.d(TAG, "Current location is null. Using defaults.");
-                            Log.e(TAG, "getPhoneLocation => Exception: %s"+ task.getException());
+                            Log.e(TAG, "getPhoneLocation => Exception: %s" + task.getException());
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
                             mMap.getUiSettings().setMyLocationButtonEnabled(false);
                         }
@@ -163,6 +177,80 @@ public class SecondMapActivity extends AppCompatActivity implements OnMapReadyCa
             }
         } catch (SecurityException e) {
             Log.e("Exception: %s", e.getMessage());
+
+        }
+    }
+
+    private void showProximityPlace() {
+        if (mMap == null) {
+            return;
+        }
+
+        if (mLocationPermissionGranted) {
+            // Get the likely places - that is, the businesses and other points of interest that
+            // are the best match for the device's current location.
+
+            @SuppressWarnings("MissingPermission") final Task<PlaceLikelihoodBufferResponse> placeResult =
+                    mPlaceDetectionClient.getCurrentPlace(null);
+
+            placeResult.addOnCompleteListener(new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+                @Override
+                public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        PlaceLikelihoodBufferResponse likelyPlace = task.getResult();
+
+                        //Store maximum entries
+                        int count;
+                        if (likelyPlace.getCount() < 10) {
+                            count = likelyPlace.getCount();
+                        } else {
+                            count = 10;
+                        }
+
+                        //Inside this loop we put marker at all position we find
+                        int i = 0;
+                        mLikelyPlaceName = new String[count];
+                        mLikelyPlaceLatlng = new LatLng[count];
+
+                        for (PlaceLikelihood placeLikelihood : likelyPlace) {
+                            mLikelyPlaceName[i] = (String) placeLikelihood.getPlace().getName();
+                            mLikelyPlaceLatlng[i] = placeLikelihood.getPlace().getLatLng();
+
+                            //Add marker in every place found
+                            if (mLikelyPlaceLatlng.length != 0) {
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(mLikelyPlaceLatlng[i])
+                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_custom_marker)));
+
+                                Log.d(TAG, "onComplete: show me Latlng marker" + mLikelyPlaceName[i] + " " + mLikelyPlaceName[i]);
+                            }
+
+                            i++;
+                            if (i > (count - 1)) {
+                                break;
+                            }
+
+                        }
+                        likelyPlace.release();
+
+                    } else {
+                        Log.e(TAG, "showProximityPlace: exception: %s", task.getException());
+                    }
+                }
+            });
+        } else {
+            //User have not granted permission
+            // The user has not granted permission.
+            Log.d(TAG, "ShowProximityPlace => The user did not grant location permission.");
+
+            // Add a default marker, because the user hasn't selected a place.
+            mMap.addMarker(new MarkerOptions()
+                    .title(getString(R.string.default_info_title))
+                    .position(mDefaultLocation)
+                    .snippet(getString(R.string.default_info_snippet)));
+
+            // Prompt the user for permission.
+            getLocationPermission();
 
         }
     }
